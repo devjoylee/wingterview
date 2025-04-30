3cafb7c793fe91f70b4a7332a059f5db39d1944d
Joy Lee <joylee.dev@gmail.com>
2025-04-30 19:34:28 +0900

    style: 로그인 페이지 UI

--- src/components/common/RootLayout/RootLayout.tsx
+++ src/components/common/RootLayout/RootLayout.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import styles from './RootLayout.module.scss'
+import wingImage from '@assets/wing.png'
+import styles from './styles.module.scss'
 
 export const RootLayout: React.FC = () => {
   return (
     <div className={styles.appContainer}>
       <div className={styles.desktopView}>
         <div className={styles.container}>
-          <img
-            src="https://github.com/user-attachments/assets/fcbe381a-189e-4d70-ac82-722e12acfd9c"
-            alt="img"
-          />
+          <img src={wingImage} alt="img" />
           <h1>WINGTERVIEW</h1>
           <p>윙터뷰, 면접에 날개를 달자!</p>
         </div>
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,17 @@
+import styles from './styles.module.scss'
+import introImage from '@assets/intro-image.png'
+
 export const LoginPage = () => {
-  return <div className="LoginPage">로그인 페이지</div>
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.introContent}>
+        <img src={introImage} alt="img" />
+        <p className={styles.greeting}>
+          모의 면접 상대를 찾고있나요? <br />
+          윙터뷰에서 매주 새로운 파트너를 만나며 <br />
+          실전 감각을 키워보세요.
+        </p>
+      </div>
+    </div>
+  )
 }
