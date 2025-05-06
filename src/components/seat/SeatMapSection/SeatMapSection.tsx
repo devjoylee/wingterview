fb55e5a2556d4cd60cd8878f2b1c767822f2a7be
Joy Lee <joylee.dev@gmail.com>
2025-05-06 13:48:48 +0900

    style: 자리배치도 라이언 기둥 위치 UI 추가

--- src/components/seat/SeatMapSection/SeatMapSection.tsx
+++ src/components/seat/SeatMapSection/SeatMapSection.tsx
@@ -11,27 +11,56 @@ export const SeatMapSection = ({
   isSeletedByMe,
   handleSeatSelect,
 }: SeatMapSectionProps) => {
+  const ryans = [
+    { name: 'A', section: 'C', row: 2 },
+    { name: 'B', section: 'C', row: 7 },
+    { name: 'C', section: 'C', row: 13 },
+    { name: 'D', section: 'A', row: 2 },
+    { name: 'E', section: 'A', row: 7 },
+    { name: 'F', section: 'A', row: 13 },
+  ]
+
   return (
     <section className={styles.section}>
       <h3 className={styles.sectionName}>{section}</h3>
-      <div className={styles.container}>
-        {Array.from({ length: 18 }, (_, row) => (
-          <div key={`${section}-${row}`} className={styles.line}>
-            <div className={styles.index}>{row + 1}</div>
+      <div className={styles.lineContainer}>
+        {Array.from({ length: 18 }, (_, row) => {
+          const ryan = ryans.find(
+            r => r.section === section && r.row === row + 1
+          )
 
-            {Array.from({ length: 3 }, (_, col) => (
-              <div
-                key={`${section}-${row}-${col}`}
-                className={`${styles.seat} ${
-                  isSeletedByMe({ section, row, col }) ? styles.selected : ''
-                }`}
-                onClick={() => handleSeatSelect({ section, row, col })}
-              >
-                {['L', 'M', 'R'][col]}
+          return (
+            <div key={`${section}-${row}`} className={styles.line}>
+              {section !== 'B' && (
+                <div className={styles.lineNumber}>{row + 1}</div>
+              )}
+
+              <div className={styles.seatWrapper}>
+                {Array.from({ length: 3 }, (_, col) => (
+                  <div
+                    key={`${section}-${row}-${col}`}
+                    className={`${styles.seat} ${
+                      isSeletedByMe({ section, row, col })
+                        ? styles.selected
+                        : ''
+                    }`}
+                    onClick={() => handleSeatSelect({ section, row, col })}
+                  >
+                    {['L', 'M', 'R'][col]}
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        ))}
+
+              {ryan ? (
+                <div className={styles.ryan}>
+                  RYAN <span>{ryan.name}</span>
+                </div>
+              ) : (
+                <div className={styles.spacer}></div>
+              )}
+            </div>
+          )
+        })}
       </div>
     </section>
   )
