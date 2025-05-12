b3f01eaf7b6d1e036d12d0735448d145d46b4e83
Joy Lee <joylee.dev@gmail.com>
2025-05-12 15:52:05 +0900

    fix: profileImageUrl 빈 값 타입 null에서 string으로 변경

--- src/stores/profileStore.ts
+++ src/stores/profileStore.ts
@@ -18,7 +18,7 @@ interface ProfileState {
   updateBasicInfo: (name: string, nickname: string, curriculum: string) => void
   updateJobInterest: (interests: string[]) => void
   updateTechStack: (stacks: string[]) => void
-  updateProfileImage: (url: string | null) => void
+  updateProfileImage: (url: string) => void
   updateSeatPosition: (seatPosition: SeatData) => void
   setSelectedSeat: (seat: SeatData) => void
 
@@ -36,7 +36,7 @@ const initialFormData: ProfileFormData = {
   curriculum: '',
   jobInterest: [],
   techStack: [],
-  profileImageUrl: null,
+  profileImageUrl: '',
   seatPosition: {
     section: '',
     seat: [null, null],
