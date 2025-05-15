cd68875762b2f9573baea8779740af0f2aa05335
Joy Lee <joylee.dev@gmail.com>
2025-05-16 01:44:14 +0900

    refactor: 인터뷰 store 상태 값 세션스토리지에 저장하는 로직으로 수정

--- src/stores/interviewStore.ts
+++ src/stores/interviewStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist, StorageValue } from 'zustand/middleware'
 
 interface InterviewState {
   interviewId: string
@@ -16,27 +17,48 @@ interface InterviewState {
   saveSelectedQuestion: (q: string) => void
 }
 
-export const useInterviewStore = create<InterviewState>(set => ({
-  interviewId: '',
-  currentRound: 1,
-  currentPhase: 'PENDING',
-  isInterviewer: false,
-  isAiInterview: false,
-  partner: null,
-  timeRemain: 0,
-  questionIdx: 1,
-  selectedQuestion: '',
-  questionOption: [],
+export const useInterviewStore = create<InterviewState>()(
+  persist(
+    set => ({
+      interviewId: '',
+      currentRound: 1,
+      currentPhase: 'PENDING',
+      isInterviewer: false,
+      isAiInterview: false,
+      partner: null,
+      timeRemain: 0,
+      questionIdx: 1,
+      selectedQuestion: '',
+      questionOption: [],
 
-  setInterviewData: data =>
-    set(state => ({
-      ...state,
-      ...data,
-    })),
+      setInterviewData: data =>
+        set(state => ({
+          ...state,
+          ...data,
+        })),
 
-  saveSelectedQuestion: (selected: string) =>
-    set(state => ({
-      selectedQuestion: selected,
-      questionIdx: state.questionIdx + 1,
-    })),
-}))
+      saveSelectedQuestion: (selected: string) =>
+        set(state => ({
+          selectedQuestion: selected,
+          questionIdx: state.questionIdx === -1 ? 1 : state.questionIdx + 1,
+        })),
+    }),
+    {
+      name: 'interview-storage',
+      storage: {
+        getItem: key => {
+          const value = sessionStorage.getItem(key)
+          return value
+            ? (JSON.parse(value) as StorageValue<InterviewState>)
+            : null
+        },
+        setItem: (key, value) => {
+          sessionStorage.setItem(key, JSON.stringify(value))
+        },
+        removeItem: key => {
+          sessionStorage.removeItem(key)
+        },
+      },
+    }
+  )
+)
