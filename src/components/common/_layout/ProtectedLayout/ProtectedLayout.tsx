3fe523f8c666217e55ee64c9c8db4ce78edc59c7
Joy Lee <joylee.dev@gmail.com>
2025-05-28 20:54:38 +0900

    hotfix: 최초 로그인한 사용자 redirect 에러 수정

--- src/components/common/_layout/ProtectedLayout/ProtectedLayout.tsx
+++ src/components/common/_layout/ProtectedLayout/ProtectedLayout.tsx
@@ -1,12 +1,28 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { useAuthStore } from '@/stores/authStore'
 
 export const ProtectedLayout: React.FC = () => {
   const { isLoggedIn, isNewUser, hasProfile } = useAuthStore()
+  const navigate = useNavigate()
+  const location = useLocation()
 
-  if (!isLoggedIn) return <Navigate to="/login" replace />
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/login', { replace: true })
+      return
+    }
 
-  if (isNewUser && !hasProfile) return <Navigate to="/profile-setup" replace />
+    if (isNewUser && !hasProfile) {
+      navigate('/profile-setup', { replace: true })
+      return
+    }
+
+    if (location.pathname === '/profile-setup' && (!isNewUser || hasProfile)) {
+      navigate('/', { replace: true })
+      return
+    }
+  }, [isLoggedIn, isNewUser, hasProfile, navigate, location.pathname])
 
   return <Outlet />
 }
