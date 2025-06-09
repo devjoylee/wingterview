b28a8986bdeee1552c3a249ee97ec3819d48aa7e
Joy Lee <joylee.dev@gmail.com>
2025-06-09 17:24:50 +0900

    style: 서브페이지 Header 컴포넌트 UI

--- src/pages/mypage/MyInterviewPage/MyInterviewPage.tsx
+++ src/pages/mypage/MyInterviewPage/MyInterviewPage.tsx
@@ -1,8 +1,10 @@
+import { SubPageHeader } from '@/components/ui'
 import styles from './styles.module.scss'
 
 export const MyInterviewPage: React.FC = () => {
   return (
     <div className={styles.myInterviewPage}>
+      <SubPageHeader name="나의 면접 회고하기" backTo="/mypage" />
       <div className={styles.container}></div>
     </div>
   )
--- src/pages/mypage/MyQuizPage/MyQuizPage.tsx
+++ src/pages/mypage/MyQuizPage/MyQuizPage.tsx
@@ -1,8 +1,10 @@
+import { SubPageHeader } from '@/components/ui'
 import styles from './styles.module.scss'
 
 export const MyQuizPage: React.FC = () => {
   return (
     <div className={styles.myQuizPage}>
+      <SubPageHeader name="나의 퀴즈 복습하기" backTo="/mypage" />
       <div className={styles.container}></div>
     </div>
   )
