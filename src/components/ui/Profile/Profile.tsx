992471168d7a756c0c278b67019a11ebeb22adff
Joy Lee <joylee.dev@gmail.com>
2025-06-16 15:06:40 +0900

    style: 카테부 유저와 일반유저 구분하여 프로필 컴포넌트 생성

---
+++ src/components/ui/Profile/Profile.tsx
@@ -0,0 +1,77 @@
+import { ProfileImage, StaticTag } from '@/components/ui'
+import ryanImage from '@assets/ryan.png'
+import styles from './styles.module.scss'
+
+interface ProfileProps {
+  profileData: ProfileCardData
+}
+
+export const Profile: React.FC<ProfileProps> = ({ profileData }) => {
+  const {
+    profileImageUrl,
+    name,
+    nickname,
+    curriculum,
+    seatCode,
+    isKTB,
+    jobInterest,
+    techStack,
+  } = profileData
+
+  return (
+    <div className={styles.profile}>
+      <div className={styles.profileHeader}>
+        <div className={styles.thumbnail}>
+          <ProfileImage url={profileImageUrl} size={72} />
+        </div>
+
+        <ul className={styles.myInfoList}>
+          <li className={styles.item}>
+            <span>이름 : </span>
+            {name}
+          </li>
+          <li className={styles.item}>
+            <span>닉네임 : </span>
+            {nickname}
+          </li>
+
+          {isKTB ? (
+            <>
+              <li className={`${styles.item} ${styles.curriculum}`}>
+                <img src={ryanImage} alt="" className={styles.ryan} />
+                <span>{curriculum} 과정</span>
+              </li>
+              <li className={styles.item}>
+                <span>자리 : {seatCode}</span>
+              </li>
+            </>
+          ) : (
+            <li className={styles.item}>
+              <span>일반 회원</span>
+            </li>
+          )}
+        </ul>
+      </div>
+
+      <div className={styles.profileBody}>
+        <div className={styles.section}>
+          <h3 className={styles.title}>희망 직무</h3>
+          <div className={styles.tagList}>
+            {jobInterest.map((job, index) => (
+              <StaticTag key={index} label={job} />
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.section}>
+          <h3 className={styles.title}>기술 스택</h3>
+          <div className={styles.tagList}>
+            {techStack.map((tech, index) => (
+              <StaticTag key={index} label={tech} dark />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
